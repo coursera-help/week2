b31ed9ccc884f905fbd46e90e8a2e1a65c610369
coursera-help <38988650+coursera-help@users.noreply.github.com>
2018-05-11 21:24:52 +0530

    Add files via upload

---
+++ PCA.R
@@ -0,0 +1,52 @@
+library(calibrate)
+library(FactoMineR)
+setwd("C://Users//tejas//Desktop")
+data_iris<- read.csv("stud.csv")
+head(data_iris)
+colSums(is.na(data_iris))
+data_iris_1<- data_iris[,-1]
+
+# Scale the data
+# standardize <- function(x) {(x - mean(x))}
+# data_iris.scaled <- apply(data_iris_1,2,function(x) (x-mean(x)))
+
+# Find Eigen values & Eigen Vector of covariance matrix
+cor<- cor(data_iris_1)
+cov <- cov(data_iris_1)
+eigen <- eigen(cov)
+rownames(eigen$vectors) <- c("anx","agora","arach","adven","extra","social")
+colnames(eigen$vectors) <- c("PC1","PC2","PC3","PC4","PC5","PC6")
+eigen
+
+# Note that the sum of the eigen values equals the total variance of the data
+sum(eigen$values)
+var(data_iris_1[,1]) + var(data_iris_1[,2])+ var(data_iris_1[,3])+ var(data_iris_1[,4])+ var(data_iris_1[,5])+ var(data_iris_1[,6])
+
+# The Eigen vectors are the principal components. We see to what extent each variable contributes
+loadings <- eigen$vectors
+pca_model <- prcomp(data_iris_1, scale. = T)                # pca_model<- PCA(data_iris_1)
+////////////////////PCA(data_iris_1)$eig //////////////////////////
+attributes(pca_model)
+summary(pca_model)
+pca_model$rotation
+
+pca.var <- pca_model$sdev^2
+pro_varx <- pca.var/sum(pca.var)
+#This shows that first principal component explains 73% variance. 
+#Second component explains 23% variance. Together
+
+
+# Visualization
+biplot(pca_model,scale = 0) 
+
+# Scree Plot
+plot(cumsum(pro_varx), 
+     xlab = "Principle Component",
+     ylab = "Cummulative Proportion of variance explained",
+     type = "b")
+
+
+
+# Predict
+pca_pred<- predict(pca_model)
+
